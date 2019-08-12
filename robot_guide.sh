0c7ce527a1ecea56961a7d58a87b084a1bb379e9
franciscoD1202 <36548559+franciscoD1202@users.noreply.github.com>
2019-08-12 16:28:26 -0500

    Update robot_guide.sh

--- robot_guide.sh
+++ robot_guide.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 clear
 echo "Starting set up" 
+
 source /opt/ros/kinetic/setup.bash
+#Edit the following line to match your /home/XXX device name.
 source /home/francisco/catkin_ws/devel/setup.bash
 
+# Comment this lines if you are not using a serial connection to the robot.
 sudo usermod -a -G dialout $USER
 sudo chmod a+rw /dev/ttyUSB0
 sudo chmod a+rw /dev/ttyUSB1
 
-# Edit this ip address for the ip of the device running the program
+# Edit this ip address for the ip of the device running the program.
 export ROS_MASTER_URI=http://157.253.207.241:11311
 ROS_MASTER_URI=http://157.253.207.241:11311
 
